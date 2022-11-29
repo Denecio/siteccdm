b2e7d59d2f6ccfe3d3759b295ac8a92ef083c6d8
Denecio <vascopereirabastos@gmail.com>
2022-11-29 14:42:24 +0000

    Mudei  as coisas de sitio para se entender melhor

--- Projeto/sketch.js
+++ Projeto/sketch.js
@@ -1,61 +1,11 @@
-class Linha {
-
-  constructor(cenas, range, x, y, vida) {
-    this.cenas = cenas;
-    this.range = range;
-    this.ax=[];
-    this.ay=[];
-    this.vida = vida;
-    this.morrer=false;
-    for ( let i = 0; i < this.cenas; i++ ) {
-      this.ax[i] = x;
-      this.ay[i] = y;
-    }
-  }
-  
-  desenha(){
-    for ( let i = 1; i < this.cenas; i++ ) {
-      this.ax[i - 1] = this.ax[i];
-      this.ay[i - 1] = this.ay[i];
-    }
-    
-    let a =random(-this.range, this.range);
-    let b =random(-this.range, this.range);
-   
-    if(this.vida>0){
-      this.ax[this.cenas - 1] += a;
-      this.ay[this.cenas - 1] += b;
-    }
-    // Constrain all points to the screen
-    
-    this.c=pg.get(this.ax[this.cenas - 1],this.ay[this.cenas - 1]);
-    this.c1=color(this.c);
-    this.c2=color(125);
-    
-    //(this.c1.toString());
-    if(this.c1.toString()==this.c2.toString()){
-      this.morrer=true;
-    }
-    
-    if(this.morrer){
-      this.vida-=1;
-    }
-
-    // Draw a line connecting the points
-    for ( let j = 1; j < this.cenas; j++ ) {
-      let val = j / this.cenas * 204.0 + 51;
-      stroke(255-val);
-      line(this.ax[j - 1]+(width/2-len/2), this.ay[j - 1]+(height/2-fontsize/2), this.ax[j]+(width/2-len/2), this.ay[j]+(height/2-fontsize/2));
-    }
-  }
-}
+let txt="ccdm"; //texto que é apresentado
+let num = 115; //número de linhas total  
+let tracos = 100; //número de tracos por linha
+let andamento=5; //quanto é que cada linha anda para o lado
+let fontsize=400;
 
 let linhas = [];
-let num = 115;
-let tracos = 100;
 let pg;
-let fontsize=400;
-let txt="ccdm"; //texto que é apresentado
 let len;
 let myFont;
 let counter=0;
@@ -118,6 +68,58 @@ function criarlinhas(){
   let cor2=color(0);
 
   if(cor.toString()==cor2.toString()){
-    linhas.push(new Linha(tracos, 5, x, y, 2));
+    linhas.push(new Linha(tracos, andamento, x, y, 2));
+  }
+}
+
+class Linha {
+
+  constructor(cenas, range, x, y, vida) {
+    this.cenas = cenas;
+    this.range = range;
+    this.ax=[];
+    this.ay=[];
+    this.vida = vida;
+    this.morrer=false;
+    for ( let i = 0; i < this.cenas; i++ ) {
+      this.ax[i] = x;
+      this.ay[i] = y;
+    }
+  }
+  
+  desenha(){
+    for ( let i = 1; i < this.cenas; i++ ) {
+      this.ax[i - 1] = this.ax[i];
+      this.ay[i - 1] = this.ay[i];
+    }
+    
+    let a =random(-this.range, this.range);
+    let b =random(-this.range, this.range);
+   
+    if(this.vida>0){
+      this.ax[this.cenas - 1] += a;
+      this.ay[this.cenas - 1] += b;
+    }
+    // Constrain all points to the screen
+    
+    this.c=pg.get(this.ax[this.cenas - 1],this.ay[this.cenas - 1]);
+    this.c1=color(this.c);
+    this.c2=color(125);
+    
+    //(this.c1.toString());
+    if(this.c1.toString()==this.c2.toString()){
+      this.morrer=true;
+    }
+    
+    if(this.morrer){
+      this.vida-=1;
+    }
+
+    // Draw a line connecting the points
+    for ( let j = 1; j < this.cenas; j++ ) {
+      let val = j / this.cenas * 204.0 + 51;
+      stroke(val);
+      line(this.ax[j - 1]+(width/2-len/2), this.ay[j - 1]+(height/2-fontsize/2), this.ax[j]+(width/2-len/2), this.ay[j]+(height/2-fontsize/2));
+    }
   }
 }
