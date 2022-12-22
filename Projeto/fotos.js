a44afadccb3b90c9d0e506704c9de278bbc4b092
Denecio <vascopereirabastos@gmail.com>
2022-12-22 19:44:21 +0000

    YOOOO

--- Projeto/fotos.js
+++ Projeto/fotos.js
@@ -1,75 +1,89 @@
-class Linha2 {
+const fotos = ( p ) => {
+  class Linha2 {
 
-    constructor(x, y, size) {
+    constructor(x, y, size, img) {
         this.pos=[];
         this.vel=[];
+        this.cor=[];
         this.size=size;
-        this.acc = createVector(0, 0);
-        this.pos.push(createVector(x, y));
-        this.vel.push(createVector(0, 0));
-        this.xoff = random(1000);
+        this.img=img;
+        this.acc = p.createVector(0, 0);
+        this.pos.push(p.createVector(x, y));
+        this.vel.push(p.createVector(0, 0));
+        this.cor.push(this.img.get(this.pos[0].x,this.pos[0].y));
+        this.xoff = p.random(1000);
     }
     
     desenha(){
         let ult=this.pos.length-1;
         if(ult+1<this.size){
-            this.pos.push(createVector(this.pos[ult].x, this.pos[ult].y));
-            this.vel.push(createVector(this.vel[ult].x, this.vel[ult].y));
+            this.pos.push(p.createVector(this.pos[ult].x, this.pos[ult].y));
+            this.vel.push(p.createVector(this.vel[ult].x, this.vel[ult].y));
+            this.cor.push(this.img.get(this.pos[ult].x,this.pos[ult].y));
             ult++;
+        } else {
+            this.pos.shift();
+            this.vel.shift();
+            this.cor.shift();
+            ult--;
         }
-     
-        let angle = noise(this.xoff) * TWO_PI * 4;
+
+        let angle = p.noise(this.xoff) * p.TWO_PI * 4;
         this.acc= p5.Vector.fromAngle(angle, 1);
    
         this.vel[ult].add(this.acc);
-        this.vel[ult].limit(3);
+        this.vel[ult].limit(2);
 
-
-        if(this.vida>0){
         this.pos[ult].add(this.vel[ult]);
-        }
 
         this.acc.mult(0);
-      
-        this.c=img.get(this.pos[ult].x,this.pos[ult].y);
+        
+        this.pos[ult].x=p.constrain(this.pos[ult].x,0,p.width);
+        this.pos[ult].y=p.constrain(this.pos[ult].y,0,p.height);
+
+        this.c=this.img.get(this.pos[ult].x,this.pos[ult].y);
   
         // Draw a line connecting the points
-        for ( let j = 1; j < this.cenas; j++ ) {
-            stroke(this.cor[j]);
-            line(this.pos[j - 1].x, this.pos[j - 1].y, this.pos[j].x, this.pos[j].y);
+        for ( let j = 1; j < ult-1; j++ ) {
+            p.stroke(p.color(this.cor[j]));
+            p.line(this.pos[j - 1].x, this.pos[j - 1].y, this.pos[j].x, this.pos[j].y);
         }
         this.xoff += 0.02;
     }
 }
 
-let ls = [];
-let numero = 200;
-let img;
-let dim;
+  let img;
 
-function preload() {
-  img = loadImage('images/astrid.jpg');
-}
+  let ls = [];
+  let numero = 100;
 
-function setup() {
-  let img1=createCanvas(img.width, img.height);    
-  img1.parent("img_astrid");
-  strokeWeight(2);
-  for (let i =0; i<numero; i++){
-    criarlinhas();
+  p.preload= function() {
+    img = p.loadImage('images/astrid400.png');
   }
-}
 
-function draw(){
-  background(255);
-  for(let i = 0; i< numero; i++){
-    ls[i].desenha();
+  p.setup = function() {
+    p.createCanvas(img.width, img.height);
+    p.strokeWeight(2);
+    //image(img,0,0);
+    for (let i =0; i<numero; i++){
+      p.criarlinhas();
+    }
+    p.background(0);
+  };
+
+  p.draw = function() {
+    //p.clear();
+    for(let i = 0; i< numero; i++){
+      ls[i].desenha();
+    }
+  };
+
+  p.criarlinhas = function(){
+    const x = p.random(p.width);
+    const y = p.random(p.height);
+    let c=img.get(x,y);
+    ls.push(new Linha2(x, y, 100, img));
   }
-}
+};
 
-function criarlinhas(){
-  const x = random(img.width);
-  const y = random(img.height);
-  let c=img.get(x,y);
-  ls.push(new Linha2(x, y, 100));
-}
+let _fotos = new p5(fotos,"img_astrid");
--- Projeto/sketch.js
+++ Projeto/sketch.js
@@ -1,167 +1,137 @@
-let txt="ccdm"; //texto que é apresentado
-let num = 225; //número de linhas total  
-let fontsize= 450;
-let vida=2;
+const ccdm = ( q ) => {
+  class Linha {
 
-let linhas = [];
-let pg;
-let len;
-let myFont;
-let counter=0;
-let a=0;
-
-let f;
-
-function preload() {
-  myFont = loadFont('../Projeto/fontes/Jost-Black.ttf');
-}
-
-function setup() {
-  noCursor();
-  var canvas = createCanvas(window.innerWidth, window.innerHeight);
-  canvas.parent("canvas");
-  textSize(fontsize);
-  let bbox = myFont.textBounds(txt, 0, fontsize, fontsize);
-  len=bbox.w+20;
-  alt=bbox.h+10;
-  pg = createGraphics(len, alt);
-  pg.background(125);
-  pg.textSize(fontsize);
-  pg.textFont(myFont);
-  pg.fill(0);
-  pg.textAlign(CENTER);
-  pg.text(txt, pg.width/2, fontsize-bbox.y+5);
+    constructor(x, y, vida) {
+      this.pos=[];
+      this.vel=[];
+      this.acc = q.createVector(0, 0);
+      this.vida = vida;
+      this.morrer=false;
+      this.pos.push(q.createVector(x, y));
+      this.vel.push(q.createVector(0, 0));
+      this.xoff = q.random(1000);
+    }
+    
+    desenha(){
+      let ult=this.pos.length-1;
+      if(!this.morrer){
+        this.pos.push(q.createVector(this.pos[ult].x, this.pos[ult].y));
+        this.vel.push(q.createVector(this.vel[ult].x, this.vel[ult].y));
+        ult++;
+      }
+      
+      let angle = q.noise(this.xoff) * q.TWO_PI * 4;
+      this.acc= p5.Vector.fromAngle(angle, 1);
+     
+      this.vel[ult].add(this.acc);
+      this.vel[ult].limit(3);
   
-  f=new fl();
-
-  while(linhas.length<num){
-      criarlinhas();
-  }
-}
-
-function draw(){
-  colorMode(RGB);
-
-  clear();
-
-  while(linhas.length<num)  {
-    criarlinhas();
-  }
-
-  /*let a=floor(frameRate());
-  fill(255,0,0);
-  textSize(40);
-  text(a, 10, 40);
-  text(linhas.length, 10, 80);*/
-
-  for(let i = linhas.length-1; i>=0; i--){
-    linhas[i].desenha();
-    if(linhas[i].pos.length==1 && linhas[i].morrer){
-      linhas.splice(i,1);
-    }
-  }
-
-  f.desenha();
-}
-
-function criarlinhas(){
-  const x = random(len);
-  const y = random(fontsize);
-
-  let c=pg.get(x,y);
-  let cor=color(c);
-  let cor2=color(0);
-
-  if(cor.toString()==cor2.toString()){
-    linhas.push(new Linha(x, y, vida));
-  }
-}
-
-function mousePressed(){
-  noLoop();
-}
-
-class Linha {
-
-  constructor(x, y, vida) {
-    this.pos=[];
-    this.vel=[];
-    this.acc = createVector(0, 0);
-    this.vida = vida;
-    this.morrer=false;
-    this.pos.push(createVector(x, y));
-    this.vel.push(createVector(0, 0));
-    this.xoff = random(1000);
-  }
   
-  desenha(){
-    let ult=this.pos.length-1;
-    if(!this.morrer){
-      this.pos.push(createVector(this.pos[ult].x, this.pos[ult].y));
-      this.vel.push(createVector(this.vel[ult].x, this.vel[ult].y));
-      ult++;
-    }
-    
-    let angle = noise(this.xoff) * TWO_PI * 4;
-    this.acc= p5.Vector.fromAngle(angle, 1);
-   
-    this.vel[ult].add(this.acc);
-    this.vel[ult].limit(3);
-
-
-    if(this.vida>0){
-      this.pos[ult].add(this.vel[ult]);
-    }
-
-    this.acc.mult(0);
-
-    this.c=pg.get(this.pos[ult].x,this.pos[ult].y);
-    this.c1=color(this.c);
-    this.c2=color(125);
-    
-    //(this.c1.toString());
-    if(this.c1.toString()==this.c2.toString()){
-      this.morrer=true;
-    }
-
-    
-    if(this.morrer){
-      this.vida-=1;
-      this.pos.splice(0,1);
-      this.vel.splice(0,1);
-    }
-
-    // Draw a line connecting the points
-    for ( let j = 1; j < this.pos.length; j++ ) {
-      strokeWeight(1);
-      let val = j*255 / this.pos.length;
-      stroke(val+50);
-      line(this.pos[j - 1].x+(width/2-len/2), this.pos[j - 1].y+(height/2-fontsize/2), this.pos[j].x+(width/2-len/2), this.pos[j].y+(height/2-fontsize/2));
-    }
-    this.xoff += 0.02;
-  } 
-}
-
-class fl{
-
-  constructor() {
-    this.pos=[];
-    for(let i=0; i<10; i++){
-      this.pos[i] = createVector(mouseX, mouseY);
-    }
+      if(this.vida>0){
+        this.pos[ult].add(this.vel[ult]);
+      }
+  
+      this.acc.mult(0);
+  
+      this.c=pg.get(this.pos[ult].x,this.pos[ult].y);
+      this.c1=q.color(this.c);
+      this.c2=q.color(125);
+      
+      //(this.c1.toString());
+      if(this.c1.toString()==this.c2.toString()){
+        this.morrer=true;
+      }
+  
+      
+      if(this.morrer){
+        this.vida-=1;
+        this.pos.splice(0,1);
+        this.vel.splice(0,1);
+      }
+  
+      // Draw a line connecting the points
+      for ( let j = 1; j < this.pos.length; j++ ) {
+        q.strokeWeight(1);
+        let val = j*255 / this.pos.length;
+        q.stroke(val+50);
+        q.line(this.pos[j - 1].x+(q.width/2-len/2), this.pos[j - 1].y+(q.height/2-fontsize/2), this.pos[j].x+(q.width/2-len/2), this.pos[j].y+(q.height/2-fontsize/2));
+      }
+      this.xoff += 0.02;
+    } 
   }
 
-  desenha(){
-    colorMode(HSB,360,100,100);
+  let txt="ccdm"; //texto que é apresentado
+  let num = 225; //número de linhas total  
+  let fontsize= 450;
+  let vida=2;
 
-    this.pos.push(createVector(mouseX, mouseY));
-    this.pos.splice(0,1);
+  let linhas = [];
+  let pg;
+  let len;
+  let myFont;
+  let counter=0;
+  let a=0;
 
-    for ( let i = 1; i < this.pos.length; i++ ) {
-      strokeWeight(3);
-      let val = (82*i)/ this.pos.length;
-      stroke(359,87,val);
-      line(this.pos[i - 1].x, this.pos[i - 1].y, this.pos[i].x, this.pos[i].y);
+  q.preload= function() {
+    myFont = q.loadFont('../Jost-Black.ttf');
+  }
+
+  q.setup = function() {
+    q.noCursor();
+    q.createCanvas(window.innerWidth, window.innerHeight);
+    q.textSize(fontsize);
+    let bbox = myFont.textBounds(txt, 0, fontsize, fontsize);
+    len=bbox.w+20;
+    alt=bbox.h+10;
+    pg = q.createGraphics(len, alt);
+    pg.background(125);
+    pg.textSize(fontsize);
+    pg.textFont(myFont);
+    pg.fill(0);
+    pg.textAlign(q.CENTER);
+    pg.text(txt, pg.width/2, fontsize-bbox.y+5);
+    
+
+    while(linhas.length<num){
+        q.criarlinhas();
+    }
+  };
+
+  q.draw = function() {
+    q.colorMode(q.RGB);
+
+    q.clear();
+  
+    while(linhas.length<num)  {
+      q.criarlinhas();
+    }
+  
+    /*let a=floor(frameRate());
+    fill(255,0,0);
+    textSize(40);
+    text(a, 10, 40);
+    text(linhas.length, 10, 80);*/
+  
+    for(let i = linhas.length-1; i>=0; i--){
+      linhas[i].desenha();
+      if(linhas[i].pos.length==1 && linhas[i].morrer){
+        linhas.splice(i,1);
+      }
+    }
+  };
+
+  q.criarlinhas = function(){
+    const x = q.random(len);
+    const y = q.random(fontsize);
+
+    let c=pg.get(x,y);
+    let cor=q.color(c);
+    let cor2=q.color(0);
+
+    if(cor.toString()==cor2.toString()){
+      linhas.push(new Linha(x, y, vida));
     }
   }
-}
+};
+
+let _ccdm = new p5(ccdm,"canvas");
