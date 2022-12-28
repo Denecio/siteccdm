ae9cd9c74efbb4a3e192f100d1b170c2faa04e81
jnpereira02 <jjooaoo.pereira@gmail.com>
2022-12-28 19:07:25 +0000

    menu

---
+++ Projeto/menu.js
@@ -0,0 +1,20 @@
+var posAnterior = window.pageYOffset;
+
+console.log("hi");
+
+
+window.onscroll = hidemenu;
+  
+  function hidemenu() {
+  var posAtual = window.pageYOffset;
+    
+     if (window.matchMedia("(min-width: 800px)").matches && (posAnterior < posAtual)) {
+     document.querySelector("header").style.top= "-73px";
+     console.log("down");
+  } else {
+    console.log("up");
+document.querySelector("header").style.top= "0px";
+ }
+    
+  posAnterior = posAtual;
+}
